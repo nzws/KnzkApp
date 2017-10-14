403b6314020ffa1d0a10d4990719231d94176ca3
yuzulabo <git-yuzu@svk.jp>
2017-10-14 11:41:47 +0900

    Music関連の追加 点滅されない問題を修正

--- www/js/music.js
+++ www/js/music.js
@@ -1,13 +1,19 @@
 function music_set(id,force) {
     var obj = document.getElementById(id);
     var audio = document.getElementById("music-control");
-    var mode, churl;
+    var mode, churl, fmode;
 
     if (force === true) {
+        if (audio.src) { //再生中
+            audio.pause();
+            audio.src = "";
+            fmode = true;
+        }
         if (obj.className !== "ons-icon fa-play fa") {
             mode = 1;
         }
     } else {
+        fmode = true;
         if (obj.className === "ons-icon fa-play fa") {
             obj.className = "ons-icon fa-stop fa";
             mode = 1;
@@ -17,15 +23,19 @@ function music_set(id,force) {
         }
     }
 
-
-    var chmode = document.getElementsByName("channel");
-    console.log(chmode.value);
+    var chmode = document.getElementById("music-form").channel;
     if (chmode.value === "ncs") churl = "http://www.agngaming.com:8000/stream/1/";
-    else churl = "https://listen.moe/stream";
+    else if (chmode.value === "listen-moe") churl = "https://listen.moe/stream";
+    else if (chmode.value === "Poolside-fm") churl = "http://stream.radio.co/s98f81d47e/listen";
+    else if (chmode.value === "dubplate-fm") churl = "http://sc2.dubplate.fm:5000/DnB/192";
+    else if (chmode.value === "FUTURE-BASS-MIX") churl = "http://stream.zenolive.com/am16uk1f4k5tv";
+    else if (chmode.value === "TOP-40-RU") churl = "http://stream.europeanhitradio.com:8000/Stream_35.aac";
+    else if (chmode.value === "REMIXES-RU") churl = "http://stream.europeanhitradio.com:8000/Stream_33.aac";
+    else if (chmode.value === "danceradiouk") churl = "http://uk2.internet-radio.com:8145/";
 
     if (mode === 1) {
         audio.src = churl;
-        if (!force) audio.play();
+        if (fmode) audio.play();
     } else {
         audio.pause();
         audio.src = "";
