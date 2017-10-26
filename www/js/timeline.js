ff264cbccb6742fb0868c49e435ef989938fa075
yuzulabo <git-yuzu@svk.jp>
2017-10-26 21:14:50 +0900

    +ローカルTL(Beta) / ジョークモードの追加

--- www/js/timeline.js
+++ www/js/timeline.js
@@ -173,8 +173,8 @@ function showTL(mode, reload, more_load, clear_load) {
             tlmode = "public?local=true&since_id="+toot_new_id;
         n = true;
     } else if (mode === "plus_local") {
-        id_main = "plus_local_main";
         if (old_TL_ws) old_TL_ws.close();
+        if (ws2) ws2.close();
 
         ws = new WebSocket("wss://"+inst+"/api/v1/streaming/?access_token=" + localStorage.getItem('knzk_login_token') + "&stream=public:local");
         old_TL_ws = ws;
@@ -184,7 +184,19 @@ function showTL(mode, reload, more_load, clear_load) {
             ws_reshtml = JSON.parse(JSON.parse(message.data).payload);
 
             if (toot_new_id !== ws_reshtml['id'])
-                document.getElementById(id_main).innerHTML = toot_card(ws_reshtml, "full", null) + document.getElementById(id_main).innerHTML;
+                document.getElementById("plus_local_main").innerHTML = toot_card(ws_reshtml, "full", null) + document.getElementById("plus_local_main").innerHTML;
+
+            toot_new_id = ws_reshtml['id'];
+        };
+
+        ws2 = new WebSocket("wss://"+inst+"/api/v1/streaming/?access_token=" + localStorage.getItem('knzk_login_token') + "&stream=home");
+        ws2.onmessage = function (message) {
+            var ws_reshtml;
+            displayTime('update');
+            ws_reshtml = JSON.parse(JSON.parse(message.data).payload);
+
+            if (toot_new_id !== ws_reshtml['id'])
+                document.getElementById("plus_local_main").innerHTML = toot_card(ws_reshtml, "full", null) + document.getElementById("plus_local_main").innerHTML;
 
             toot_new_id = ws_reshtml['id'];
         };
@@ -220,6 +232,7 @@ function showTL(mode, reload, more_load, clear_load) {
 
                     if (!reload && !more_load) {
                         if (old_TL_ws) old_TL_ws.close();
+                        if (ws2) ws2.close();
 
                         ws = new WebSocket("wss://"+inst+"/api/v1/streaming/?access_token=" + localStorage.getItem('knzk_login_token') + "&stream=" + ws_mode);
                         old_TL_ws = ws;
