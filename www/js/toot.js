124185bede2bc73ca35b5930efbec4e035a4a07b
yuzulabo <git-yuzu@svk.jp>
2017-11-17 14:45:54 +0900

    修正

--- www/js/toot.js
+++ www/js/toot.js
@@ -309,7 +309,7 @@ function more(id, acctid, pin_mode, url) {
             buttons: [
                 '詳細を表示',
                 'ブラウザで表示',
-                '元のトゥートを表示',
+                //'元のトゥートを表示',
                 '近くのトゥートを表示',
                 {
                     label: pin,
@@ -327,10 +327,10 @@ function more(id, acctid, pin_mode, url) {
         }).then(function (index) {
             if (index == 0) show_post(more_status_id);
             else if (index == 1) window.open(url, "_blank", "enableViewportScale=yes");
-            else if (index == 2) disp_before(more_status_id, url);
-            else if (index == 3) show_post(more_status_id, true);
-            else if (index == 4) pin_set(more_status_id, pin_mode);
-            else if (index == 5) show('delete-post');
+            //else if (index == 2) disp_before(more_status_id, url);
+            else if (index == 2) show_post(more_status_id, true);
+            else if (index == 3) pin_set(more_status_id, pin_mode);
+            else if (index == 4) show('delete-post');
         })
     } else {
         ons.openActionSheet({
