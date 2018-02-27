4a500e78cd6c3a1dab8bafc66f4e7b2f7968bc77
yuzulabo <git-yuzu@svk.jp>
2018-02-27 21:52:50 +0900

    feat: インスタンス設定を別ファイルにまとめる

---
+++ www/config/instance.js
@@ -0,0 +1,33 @@
+/**
+ * インスタンス仕様設定ファイル
+ * limit: 文字数
+ * glitch_soc: Glitch-socインスタンスか
+ * bbcode: BBCode (元: now.kibousoft.co.jp) 対応か
+ * markdown: Markdown (元: kirishima.cloud) 対応か
+ * enquete: アンケート(元: friends.nico) 対応か
+ * enquete_duration: アンケートの期間設定 (元: knzk.me) 対応か
+ */
+
+const instance_config = {
+    "knzk.me": {
+        limit: 5000,
+        glitch_soc: true
+    },
+    "Knzk.me": {
+        limit: 5000,
+        glitch_soc: true
+    },
+    "now.kibousoft.co.jp": {
+        limit: 4096,
+        bbcode: true
+    },
+    "kirishima.cloud": {
+        limit: 6229,
+        bbcode: true,
+        markdown: true
+    },
+    "friends.nico": {
+        limit: 500,
+        enquete: true
+    }
+};
