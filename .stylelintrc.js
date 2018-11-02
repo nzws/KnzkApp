2ad7d113dd1773cb3c49acbae31f47bd7b0a1909
bsky <git@bsky.moe>
2018-11-02 13:37:07 +0900

    Switch extension of stylelintrc from json to js

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'selector-list-comma-newline-after': 'always-multi-line',
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignoreTypes: ['/^ons-/']
+      }
+    ]
+  }
+}
