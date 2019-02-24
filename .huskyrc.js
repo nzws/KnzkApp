877b4fa059a4284a2d0cf70568d4b48add2a3e9c
bsky <git@bsky.moe>
2019-02-24 18:18:37 +0900

    Update .huskyrc.js

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,6 +4,6 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['pretty-quick --staged', 'lint-staged'])
+    'pre-commit': tasks(['lint-staged', 'pretty-quick --staged'])
   }
 };
