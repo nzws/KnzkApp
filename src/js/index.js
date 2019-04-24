bf352019b3a8bd46622e27d251d54e4f7988f90a
nzws <git@nzws.me>
2019-04-25 01:41:11 +0900

    kiresou

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,6 @@ import $ from 'jquery';
 window.$ = $;
 
 import 'onsenui';
-import './lib/monaca-cordova-loader';
-import './lib/monaca-core-utils';
 import '../scss/index.scss';
 
 window.knzk = {};
